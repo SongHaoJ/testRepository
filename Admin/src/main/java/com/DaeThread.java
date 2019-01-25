cf77bf7ed3b779e831d0618d5c60d56b17e6b870
songhao <aaa@aaa.com>
2019-01-25 14:55:08 +0800

    1

--- Admin/src/main/java/com/DaeThread.java
+++ Admin/src/main/java/com/DaeThread.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author songhao
+ */
 @Component
 public class DaeThread extends Thread {
     private Boolean start = true;
